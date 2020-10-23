49f99abedce1316f7ba5d422d52279170d560ac3
repl.it user <replituser@example.com>
2020-10-23 10:26:25 +0000

    6

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 class Main {
 
   public static void main(String[] args) {
-    
-    int z = 0;
 
+    int z = 0;
+    int sum;
 for(int i = 1; i <= 200; i++){
    z = i;
+   sum = i+ + z;
      {
-    System.out.println(z);
+      System.out.println(z);
+      if(i %5 == 0 )
+      System.out.println("ZAHL ist durch 5 teilbar!");
+      if (i==99||i==9||i==19||i==29||i==39||i==49||i==59||i==69||i==79||i==89||i==109||i==119||i==129||i==139||i==149||i==159||i==169||i==179||i==189||i==199)
+      System.out.println("ZAHL endet auf 9!");
+      if(sum %3 == 0)
+       System.out.println("ERGEBNIS ist durch 3 teilbar");
      }
   }
   }
